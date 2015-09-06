039ddf6f0d1bbb29fc3d4245344e56938437315f
chromonav <chromonav@gmail.com>
2015-09-06 19:32:07 +0530

    added a bit ui and implemented events

--- client/qzapp.js
+++ client/qzapp.js
@@ -1,20 +1,25 @@
-if (Meteor.isClient) {
 
+var questionset
   Meteor.startup(function() {
-      $('#myAffix').affix({})
+
   });
   // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.index.helpers({
-    counter: function () {
-      return Session.get('counter');
+
+ Template.engine.helpers({
+    question: "hello"
+  });
+
+
+Session.setDefault("qview",0);
+
+ Template.engine.events({
+    "click #next": function (event) {
+      // Prevent default browser form submit
+    
+      var qans = Session.get("qview");
+      qans = qans +1;
+      Session.set("qview",qans);
+      
     }
   });
-
-  Template.index.events({
-
-  });
-
-
-}
