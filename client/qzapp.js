352bbecbfaa4740c2c5a7ab8eda19109c6706685
chromonav <chromonav@gmail.com>
2015-09-09 01:25:50 +0530

    added question view ,fixed navbar

--- client/qzapp.js
+++ client/qzapp.js
@@ -1,17 +1,24 @@
-
+var Questionbank = new Mongo.Collection("questionbank");
 var questionset
   Meteor.startup(function() {
+Session.setDefault("qview",1);
 
   });
   // counter starts at 0
+var hello = Questionbank.find({});
+Session.setDefault("qview",1);
 
 
  Template.engine.helpers({
-    question: "hello"
+   question: "hello"
+  ,  option1:"yep"
+  , option2:"hoho"
+  , option3:"lol"
+  , option4:"hehe"
+  , no:function(){return Session.get("qview") }
+
   });
 
-
-Session.setDefault("qview",0);
 
  Template.engine.events({
     "click #next": function (event) {
@@ -20,6 +27,19 @@ Session.setDefault("qview",0);
       var qans = Session.get("qview");
       qans = qans +1;
       Session.set("qview",qans);
-      
-    }
+      console.log(Session.get("qview"));
+      },
+      "click #back": function (event) {
+      // Prevent default browser form submit
+    
+      var qans = Session.get("qview");
+      qans = qans-1;
+      if(qans<1){
+          qans = 1;
+        }        
+      Session.set("qview",qans);
+            console.log(Session.get("qview"));
+
+      }
+
   });
